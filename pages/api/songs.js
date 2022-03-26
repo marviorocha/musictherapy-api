ae6395d3cbbd20f121a95380c2e0eee1321fee17
marviorocha <marviorocha@marviorocha.com>
2022-03-26 19:26:19 -0300

    Added cors to api

--- pages/api/songs.js
+++ pages/api/songs.js
@@ -1,6 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors'
+import initMiddleware from '../lib/init-middleware'
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await cors(req, res)
   const default_url = process.env.default_url;
   res.status(200).json(
 
